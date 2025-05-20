149b9983935d8e65ec69c13637aae74bc74816ea
FelipeTanusR <108429239+FelipeTanusR@users.noreply.github.com>
2025-05-20 19:23:58 -0300

    Add files via upload
    Commit inicial

---
+++ hello/stdio.h
@@ -0,0 +1,16 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 FelipeTanusR <fetanus@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  FelipeTanusR <fetanus@usp.br>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+
+#endif	
