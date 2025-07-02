e3479708dc104260a67809a26459a10a51701c75
FelipeTanusR <fetanus@usp.br>
2025-07-01 21:14:38 -0300

    primeiro unpack quiz

---
+++ quiz/p4.c
@@ -0,0 +1,19 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 FelipeTanusR <fetanus@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  FelipeTanusR <fetanus@usp.br>
+ */
+
+void foo();
+void bar();
+
+int main()
+{
+  foo();
+  return 0;
+}
